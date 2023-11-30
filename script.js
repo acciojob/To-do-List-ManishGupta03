4d498bfa4b086421108fa0312513b7be0c383beb
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-11-30 21:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
 
+//your code here
+const input =document.getElementById("newTodoInput");
+const oList=document.getElementById("todoList");
+const button=document.getElementById("addTodoBtn");
+button.addEventListener("click", onClick);
+ function onClick(){
+	if(input.value!==""){
+		let list=document.createElement("li");
+		list.innerText=input.value;
+		oList.appendChild(list);
+	}
+	input.value="";
+}
